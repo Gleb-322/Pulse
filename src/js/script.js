c896ad8510420a544ab835fe24e8871e465274c1
Gleb <gyrra98@mail.ru>
2021-04-18 22:35:06 +0300

    add tabs and started making a new section

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,40 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-      });
-  });
+/* slick slider */
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
+//       });
+//   });
+
+
+/* tiny slider */
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    // responsive: {
+
+    // }
+});
+
+document.querySelector('.prev').addEventListener ('click', function () {  /* document.querySelector('.prev') обращение к первому элементу на странице с классом .prev */
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener ('click', function () {  /* document.querySelector('.prev') обращение к первому элементу на странице с классом .prev */
+    slider.goTo('next');
+});
