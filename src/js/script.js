30fff8c8c2acabe9bac1296f935d0365657f5184
Gleb <gyrra98@mail.ru>
2021-05-01 22:40:23 +0300

    add modal windows and validation for forms

--- src/js/script.js
+++ src/js/script.js
@@ -59,4 +59,59 @@ $(document).ready(function() { /* универсальный скрипт для
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Modal 
+    /* fadeOut(); позволяет анимированно скрыть какие-то элементы на странице */
+    $('[data-modal="consultation"]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    }); 
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    
+    /* each(function(i)) перебор по всем элементам с этим тегом*/
+    /* .text() получает текст которые есть внутри .modal__descr , .text('eewfwf') заменяет текст в .modal__descr на то что в скобках*/
+    /* eq(i) позволяет получать определенный элемент по порядку */
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    /* валидация форм заполнения */
+    /* validate() работает для первой формы в feed-form , поэтому используют обращение по уникальному индификатору*/
+    /* ('#consultation form') если есть уже индификатор, то чтобы обратиться к нужному блоку можно использовать вложенность элементов */
+    
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 4
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+     
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    
+
 });
